f80ba813c53c1ae1a69d2fa9525fa6ad9e064f35
zhouzhi0128 <73263767+zhouzhi0128@users.noreply.github.com>
2021-05-11 01:24:29 +0800

    github updata

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("github 更新了 ~！test");
     }
 
 }
